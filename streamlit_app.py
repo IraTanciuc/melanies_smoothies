5814315be995d08aee6e6233ee91d8bd00303739
IraTanciuc <48104632+IraTanciuc@users.noreply.github.com>
2024-09-12 11:13:56 +1000

    Create streamlit_app.py
    copied from my Custom Smoothies SiS app

---
+++ streamlit_app.py
@@ -0,0 +1,44 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
+
+# Write directly to the app
+st.title(":cup_with_straw: Customise Your Smoothie :cup_with_straw:")
+st.write("Choose the fruits you want in your custom Smoothie!")
+
+# add name on order
+name_on_order = st.text_input('Name on Smoothie:')
+st.write("The name on your Smoothie will be:", name_on_order)
+
+session = get_active_session()
+# create a dataframe with fruit names
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'))
+# st.dataframe(data=my_dataframe, use_container_width=True)
+
+# Create a dropdown with fruit names allowing max 5 selections
+ingredients_list = st.multiselect(
+    'Choose up to 5 ingredients:'
+    , my_dataframe
+    , max_selections = 5
+)
+
+# add a submit button
+time_to_insert = st.button('Submit Order')
+
+# if there's fruits selected, add them all to ingredients_string
+if ingredients_list:
+    ingredients_string = ''
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
+
+    # st.write(ingredients_string)
+
+    # create an insert statment with the fruits and name on order
+    my_insert_stmt = """insert into smoothies.public.orders(ingredients, name_on_order)
+        values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
+
+# if someone clicked on the submit button, execute the insert sql statement and display a success message
+if time_to_insert:
+    session.sql(my_insert_stmt).collect()
+    st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
